034e37cbe13840457ddc827873108d8647fe9c45
pjmnb007 <pjmnb007@163.com>
2015-10-31 13:51:24 +0800

    add two functions in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,15 @@ window.laod=function(){
     	var e=event || window.event;
 	alert("x:"+e.pageX+" "+"y:"+e.pageY);
     }
+    window.mousedown=function(evet){
+    	var e=event | window.event;
+	alert(e.x+" "+e.y);
+    }
+	
+}
+function whichPic(){
+	for(var i=0;i<photos.length;i++){
+	   var pic=document.getElemetById('photo-'+i+'.jpg');
+		alert(pic.getAtrribut('id'));
+	}
 }
